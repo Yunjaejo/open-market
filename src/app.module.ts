f62af7dee2ba2b4bdd07da29c3afeeeded532f06
Yunjae Jo <73681656+Yunjaejo@users.noreply.github.com>
2025-02-27 20:43:56 +0900

    Feat/db connection #8 (#9)
    * chore: env.example 추가 #8
    
    * chore: configuration 의존성 추가, start:dev 커맨드 변경
    
    * feat: 환경에 따른 db 커넥션 동적 연결 #8
    
    close #8

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    PrismaModule,
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,30 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor(private configService: ConfigService) {
+    const dbHost = configService.get<string>('DATABASE_HOST'); // DATABASE_HOST
+    const dbPort = configService.get<number>('DATABASE_PORT'); // DATABASE_PORT
+    const dbUser = configService.get<string>('DATABASE_USER'); // DATABASE_USER
+    const dbPassword = configService.get<string>('DATABASE_PASSWORD'); // DATABASE_PASSWORD
+    const dbName = configService.get<string>('DATABASE_NAME'); // DATABASE_NAME
+
+    const databaseUrl = `postgresql://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`;
+
+    super({
+      datasources: {
+        db: {
+          url: databaseUrl,
+        },
+      },
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
     console.log('DB Connected!');
