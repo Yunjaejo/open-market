897d6e9db20e5e718c6a8c96ecc67f329a482dac
Yunjaejo <yunjae.dev@gmail.com>
2025-03-04 20:57:38 +0900

    chore: 환경에 따른 env 분리 스크립트 작성

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const envFile = process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev';
+  dotenv.config({ path: envFile });
   app.use(cookieParser());
   await app.listen(3000);
 }
+
 bootstrap();
